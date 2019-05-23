711f09530dab46c9d5bc7e35dbee89e82fe39f28
Daniel Tao <danieltaox@gmail.com>
2019-05-23 12:51:06 -0400

    Post wav to backend upon completion of recording

--- web/src/client/App.js
+++ web/src/client/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactMic from 'react-mic';
+import Recorder from 'recorder-js';
+import axios from 'axios';
+
+const backend = axios.create({
+  baseUrl: './authenticate',
+  timeout: 10000
+});
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const recorder = new Recorder(audioContext);
 
 export default class App extends Component {
   constructor(props) {
@@ -8,37 +16,46 @@ export default class App extends Component {
     this.state = {
       record: false
     };
-    this.onStop = this.onStop.bind(this);
-    this.stopRecording = this.stopRecording.bind(this);
-    this.startRecording = this.startRecording.bind(this);
+    this.toggleRecord = this.toggleRecord.bind(this);
+    this.postAudio = this.postAudio.bind(this);
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+      .then((stream) => {
+        recorder.init(stream);
+      });
   }
 
-  onStop = (recorded) => {
-    console.log(recorded);
+  toggleRecord = () => {
+    const { record } = this.state;
+    this.setState({ record: !record }, this.postAudio);
   }
 
-  stopRecording = () => {
-    this.setState({
-      record: false
-    });
-  }
-
-  startRecording = () => {
-    this.setState({
-      record: true
-    });
+  postAudio = () => {
+    const { record } = this.state;
+    if (!record) {
+      recorder.stop()
+        .then(({ blob }) => {
+          console.log(blob);
+          backend.post(blob, {
+            header: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }).then((response) => {
+            console.log(response);
+          });
+        });
+    }
   }
 
   render() {
     const { record } = this.state;
     return (
       <React.Fragment>
-        <ReactMic
-          record={record}
-          onStop={this.onStop}
-        />
-        <button onClick={this.startRecording} type="button" />
-        <button onClick={this.stopRecording} type="button" />
+        <button type="button" onClick={this.toggleRecord} />
+        { record ? (
+          <h1>Recording</h1>
+        ) : (
+          <h1>Not Recording</h1>
+        )}
       </React.Fragment>
     );
   }
