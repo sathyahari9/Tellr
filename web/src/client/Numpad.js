a2737add075d89a028430f7fd4e5a95961721f32
Daniel Tao <danieltaox@gmail.com>
2019-05-24 02:08:29 -0400

    Complete styled and functional keypad component

--- web/src/client/Numpad.js
+++ web/src/client/Numpad.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Button, Columns } from 'react-bulma-components';
+import { Button, Columns, Container } from 'react-bulma-components';
 import './app.css';
 
 export default class NumPad extends Component {
@@ -11,96 +11,99 @@ export default class NumPad extends Component {
     }
   }
   render() {
+    let buttons = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
     return (
-      <React.Fragment>
-        <Columns>
+      <Container style={{
+        paddingLeft: "30vw"
+      }}>
+        {buttons.map((row, rowInd) => {
+          return (
+            <Columns style={{ width: "30vw" }}>
+              {
+                row.map((num) => {
+                  return (
+                    <Columns.Column
+                      key={"row" + rowInd + num}
+                    >
+                      <Button
+                        style={{
+                          color: "#307FEA",
+                          width: "8vw",
+                          height: "8vw",
+                          fontSize: "3vw",
+                          fontWeight: "bold",
+                          borderColor: "#307FEA"
+                        }}
+                        onClick={() => {
+                          let { value } = this.state;
+                          if (value.length < 4) {
+                            this.setState({ value: value + num });
+                          }
+                        }}
+                        rounded
+                        key={"but" + num}
+                      >
+                        {num}
+                      </Button>
+                    </Columns.Column>
+                  );
+                }
+                )}
+            </Columns>
+          );
+        })}
+        <Columns style={{ width: "30vw" }}>
+          <Columns.Column
+            style={{width: "8vw"}}
+          ></Columns.Column>
           <Columns.Column>
             <Button
-              fullwidth={true}
-              inverted={true}
-              rounded={true}
+              inverted
+              style={{
+                color: "#307FEA",
+                borderColor: "#307FEA",
+                width: "8vw",
+                height: "8vw",
+                fontSize: "3vw",
+                fontWeight: "bold"
+              }}
+              onClick={() => {
+                let { value } = this.state;
+                if (value.length < 4) {
+                  this.setState({ value: value + 0 });
+                }
+              }}
+              rounded
+              key={"but" + 0}
             >
-              1
-          </Button>
-          </Columns.Column>
-          <Columns.Column>
-            <Button
-              fullwidth={true}
-              inverted={true}
-              rounded={true}
-            >
-              2
-          </Button>
-          </Columns.Column>
-          <Columns.Column>
-            <Button
-              fullwidth={true}
-              inverted={true}
-              rounded={true}
-            >
-              3
-          </Button>
-          </Columns.Column>
-        </Columns>
-        <Columns>
-          <Columns.Column>
-            <Button
-              fullwidth={true}
-              inverted={true}
-              rounded={true}
-            >
-              4
-        </Button>
-          </Columns.Column>
-          <Columns.Column>
-            <Button
-              fullwidth={true}
-              inverted={true}
-              rounded={true}
-            >
-              5
-        </Button>
-          </Columns.Column>
-          <Columns.Column>
-            <Button
-              fullwidth={true}
-              inverted={true}
-              rounded={true}
-            >
-              6
-        </Button>
-          </Columns.Column>
-        </Columns>
-        <Columns>
-          <Columns.Column>
-            <Button
-              fullwidth={true}
-              inverted={true}
-              rounded={true}
-            >
-              7
+              0
             </Button>
           </Columns.Column>
           <Columns.Column>
             <Button
-              fullwidth={true}
-              inverted={true}
-              rounded={true}
+              inverted
+              style={{
+                width: "8vw",
+                height: "8vw",
+                fontSize: "3vw",
+                fontWeight: "bold",
+                backgroundColor: "#307FEA",
+                color: "#FFFFFF",
+              }}
+              onClick={() => {
+                let { value } = this.state;
+                if (value.length > 0) {
+                  this.setState({ value: value.substr(value.length - 1) });
+                }
+              }}
+              rounded
+              key={"but" + 0}
             >
-              8
+              🠸
             </Button>
           </Columns.Column>
-          <Columns.Column>
-            <Button
-              fullwidth={true}
-              inverted={true}
-              rounded={true}
-            >
-              9
-           </Button>
-          </Columns.Column>
         </Columns>
-      </React.Fragment>
+      </Container>
     );
   }
 }
