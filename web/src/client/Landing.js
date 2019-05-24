fb2ea2f3ca590d1e7fcd2d5de1cb9722d7502a20
Daniel Tao <danieltaox@gmail.com>
2019-05-24 00:13:37 -0400

    Extract new Landing component

---
+++ web/src/client/Landing.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Tile, Heading, Image, Columns, Button } from 'react-bulma-components';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import './app.css';
+import eyeIcon from './assets/eyeHighRes.png';
+import slashIcon from './assets/eyeSlashHighRes.png';
+
+export default class Landing extends Component {
+  render() {
+    return(
+      <Columns>
+        <Columns.Column>
+          <Tile style={{
+            height: "99vh",
+            backgroundColor: "#307FEA"
+          }}>
+            <Heading style={{
+              width: "100%",
+              marginTop: "30vh",
+              color: "#FFFFFF",
+              fontWeight: "bold"
+          }}>
+            <img style={{width: "25%"}}src={slashIcon}/><br/><br/>
+            ASSISTED<br/>VERSION</Heading>
+          </Tile>
+        </Columns.Column>
+        <Columns.Column>
+          <Tile style={{ height: "98vh" }}>
+            <Heading
+              style={{
+              width: "110%",
+              marginTop: "30vh",
+              color: "#307FEA",
+              fontWeight: "bold"
+            }}
+          >
+            <img style={{width: "25%"}} src={eyeIcon}/><br/><br/>
+            NON-ASSISTED<br/>VERSION
+            <br/>
+            <div style={{float: "right", marginTop: "15vh", marginRight: "5vw"}}>
+              <Button rounded style={{height: "4rem", backgroundColor:"#307FEA", width:"20vw"}}>
+                <Heading style={{color:"#FFFFFF"}}>ENGLISH</Heading>
+              </Button>
+              <br/>
+              <Button rounded style={{height: "4rem", width:"20vw", marginTop: "2vh"}}>
+                <Heading style={{color:"#307FEA"}}>OTHER</Heading>
+              </Button>
+          </div>
+            </Heading>
+          </Tile>
+        </Columns.Column>
+      </Columns>
+  );
+  }
+}
+    
--- web/src/client/Teller.js
+++ web/src/client/Teller.js
@@ -4,60 +4,22 @@ import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './app.css';
 import eyeIcon from './assets/eyeHighRes.png';
 import slashIcon from './assets/eyeSlashHighRes.png';
-
-const landing =
-  <React.Fragment>
-    <Columns>
-      <Columns.Column>
-        <Tile style={{
-          height: "98vh",
-          backgroundColor: "#307FEA"
-        }}>
-          <Heading style={{
-            width: "100%",
-            marginTop: "30vh",
-            color: "#FFFFFF",
-            fontWeight: "bold"
-          }}>
-            <img style={{width: "25%"}}src={slashIcon}/><br/><br/>
-            ASSISTED<br/>VERSION</Heading>
-        </Tile>
-      </Columns.Column>
-      <Columns.Column>
-        <Tile style={{ height: "98vh" }}>
-          <Heading
-            style={{
-              width: "110%",
-              marginTop: "30vh",
-              color: "#307FEA",
-              fontWeight: "bold"
-            }}
-          >
-            <img style={{width: "25%"}} src={eyeIcon}/><br/><br/>
-            NON-ASSISTED<br/>VERSION
-            <br/>
-            <div style={{float: "right", marginTop: "15vh", marginRight: "5vw"}}>
-          <Button rounded style={{height: "4rem", backgroundColor:"#307FEA", width:"20vw"}}>
-            <Heading style={{color:"#FFFFFF"}}>ENGLISH</Heading>
-          </Button>
-          <br/>
-          <Button rounded style={{height: "4rem", width:"20vw", marginTop: "2vh"}}>
-            <Heading style={{color:"#307FEA"}}>OTHER</Heading>
-          </Button>
-          </div>
-          </Heading>
-        </Tile>
-      </Columns.Column>
-    </Columns>
-  </React.Fragment>
+import NumPad from './Numpad.js';
+import Landing from './Landing.js';
 
 export default class Teller extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      view: "landing"
+    }
   }
   render() {
-    return (
-      landing
-    );
+    switch(this.state.view) {
+      case "keypad":
+        return (<NumPad />);
+      default:
+        return (<Landing />);
+    }
   }
 }
