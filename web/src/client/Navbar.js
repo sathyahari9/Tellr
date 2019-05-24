783b14a44133ef2b38123a6bbb0d80d9f53e6f79
Daniel Tao <danieltaox@gmail.com>
2019-05-24 03:30:20 -0400

    Fix Navbar styling and add background color

--- web/src/client/Navbar.js
+++ web/src/client/Navbar.js
@@ -2,28 +2,52 @@ import React from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Navbar } from 'react-bulma-components';
 import './reset.css';
+import logo from './assets/logo.png';
 
 function CustomNav(props) {
     return (
-        <Navbar>
-            <Navbar.Brand>
+        <Navbar style={{
+            height: "72px"
+        }}>
+            <Navbar.Brand
+            style={{
+                height: "60px"
+            }}>
                 <Navbar.Item 
+                    style={{
+                        color: "#307FEA",
+                        height: "60"
+                    }}
                     renderAs="a" 
                     onClick={props.homeOpen}
                 >
                     <img
+                        src={logo}
                         alt="Tellr"
-                        width="30"
-                        height="30"
+                        style= {{
+                            marginTop: "12px",
+                            maxHeight: "58px"
+                        }}
+                        width="58"
+                        height="60px !important"
                     />
                 </Navbar.Item>
             </Navbar.Brand>
             <Navbar.Menu>
                 <Navbar.Container position="end">
-                    <Navbar.Item onClick={props.appOpen}>
+                    <Navbar.Item 
+                        style={{
+                            color: "#307FEA"
+                        }}
+                    onClick={props.appOpen}>
                         App
                     </Navbar.Item>
-                    <Navbar.Item onClick={props.aboutOpen} >
+                    <Navbar.Item
+                        onClick={props.aboutOpen} 
+                        style={{
+                            color: "#307FEA"
+                        }}
+                    >
                         About Us
                     </Navbar.Item>
                 </Navbar.Container>
--- web/src/client/Static.js
+++ web/src/client/Static.js
@@ -4,7 +4,7 @@ import './app.css';
 export default class Static extends Component {
   render() {
     return (
-      <div>
+      <div style={{height: "100vh", width: "99.9vw", backgroundColor: "#307FEA", overflow: "hidden"}}>
         
       </div>
     );
